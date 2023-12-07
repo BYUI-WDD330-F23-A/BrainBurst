6219f172c19efdd826c586a269a77de798732d4a
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-12-06 20:55:30 -0700

    Touch up on rules.

--- src/Rules.js
+++ src/Rules.js
@@ -8,14 +8,20 @@ export function RulesPage() {
       "The objective of this game is to answer as many questions correctly as you can."
     }),
     createElement("h2", { textContent: "Rules" }),
-    createElement("p", { textContent:
-      "You will be able to answer each question only one time. Your answer will be flagged as correct or incorrect as soon as you submit the answer. Try to do as much as you can on your own. There is no time limit."
-    }),
+    createElement("ol", {}, [
+      createElement("li", { textContent: "You will be able to answer each question only one time." } ),
+      createElement("li", { textContent: "Your answer will be flagged as correct or incorrect as soon as you submit the answer." } ),
+      createElement("li", { textContent: "There is no time limit." } ),
+      createElement("li", { textContent: "Try to do as much as you can on your own." } )
+    ]),
+    
     createElement("h2", { textContent: "Instructions" }),
     createElement("p", { textContent:
       "The quiz is multiple choice. Simply read the question and choose from the list of available answers. When you are sure of your choice, click on the answer. You will receive instant feedback if your answer was right or wrong. Click the [Next] button to advance to the next question."
     }),
   ];
+
+  console.log(theRules);
 
   const rulesContainer = createElement("div", {}, theRules);
 
