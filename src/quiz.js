e938ce2568b333df6bbf6d34d7b214de70eaab90
Edgar Cobian <edgarcobian0304@gmail.com>
2023-12-11 12:11:01 -0800

    Injected the data to HTML

--- src/quiz.js
+++ src/quiz.js
@@ -1,72 +1,85 @@
-import { createElement } from "./utils";
+import {createElement} from "./utils";
 
 function options() {
   const option1 = createElement(
     "input",
-    { name: "answer", id: "option1", type: "radio", value: "option 1" },
-    [],
+    {name: "answer", id: "option1", type: "radio", value: "option 1"},
+    []
   );
   const option2 = createElement(
     "input",
-    { name: "answer", id: "option2", type: "radio", value: "option 2" },
-    [],
+    {name: "answer", id: "option2", type: "radio", value: "option 2"},
+    []
   );
   const option3 = createElement(
     "input",
-    { name: "answer", id: "option3", type: "radio", value: "option 3" },
-    [],
+    {name: "answer", id: "option3", type: "radio", value: "option 3"},
+    []
   );
   const option4 = createElement(
     "input",
-    { name: "answer", id: "option4", type: "radio", value: "option 4" },
-    [],
+    {name: "answer", id: "option4", type: "radio", value: "option 4"},
+    []
   );
   const option5 = createElement(
     "input",
-    { name: "answer", id: "option5", type: "radio", value: "option 5" },
-    [],
+    {name: "answer", id: "option5", type: "radio", value: "option 5"},
+    []
   );
 
   const label1 = createElement(
     "label",
-    { for: "option1", textContent: "Option 1" },
-    [option1],
+    {for: "option1", textContent: "Option 1"},
+    [option1]
   );
   const label2 = createElement(
     "label",
-    { for: "option2", textContent: "Option 2" },
-    [option2],
+    {for: "option2", textContent: "Option 2"},
+    [option2]
   );
   const label3 = createElement(
     "label",
-    { for: "option3", textContent: "Option 3" },
-    [option3],
+    {for: "option3", textContent: "Option 3"},
+    [option3]
   );
   const label4 = createElement(
     "label",
-    { for: "option4", textContent: "Option 4" },
-    [option4],
+    {for: "option4", textContent: "Option 4"},
+    [option4]
   );
   const label5 = createElement(
     "label",
-    { for: "option5", textContent: "Option 5" },
-    [option5],
+    {for: "option5", textContent: "Option 5"},
+    [option5]
   );
 
   return createElement("form", {}, [label1, label2, label3, label4, label5]);
 }
+async function getData() {
+  const jsonData = await fetch("../data/triviaGoodResponses.json").then((res) =>
+    res.json()
+  );
+  console.log(jsonData.Topic);
+  return jsonData.Topic["Best general trivia questions"];
+}
+const data = await getData();
 
 export function quizContainer() {
-  const quizName = createElement("h1", { textContent: "Quiz Name" }, []);
+  // console.log(data);
+  // console.log(JSON.parse(data));
+  for (let i = 0; i < 10; i++) {
+    console.log(data[i].Question);
+  }
+  const quizName = createElement("h1", {textContent: "Quiz Name"}, []);
 
-  const question = createElement("h2", { textContent: "Question #" }, []);
+  const question = createElement("h2", {textContent: data[0].Question}, []);
 
-  const btnNext = createElement("button", { textContent: "Next" });
+  const btnNext = createElement("button", {textContent: "Next"});
   btnNext.addEventListener("click", () => {
     console.log("NEXT Question"); // When click this should take you to next question.
   });
 
-  const questionCounter = createElement("span", { textContent: `1/10` }); // this should count the question number
+  const questionCounter = createElement("span", {textContent: `1/10`}); // this should count the question number
 
   const answercontainer = createElement("div", {}, [
     quizName,
@@ -75,6 +88,7 @@ export function quizContainer() {
     questionCounter,
     btnNext,
   ]);
+  console.log(answercontainer);
 
   return answercontainer;
 }
